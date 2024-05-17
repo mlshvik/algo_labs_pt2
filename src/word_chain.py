3d6b43b9b9f9b94c12ee947bae12e5e6e1f733dc
mlshvik <viktoriia.malish.ir.2023@lpnu.ua>
2024-05-17 10:39:39 +0300

    solution to linguistic game

---
+++ src/word_chain.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestStrChain(self, words) -> int:
+        words.sort(key=len)
+        dp = {}
+        for word in words:
+            for i in range(len(word)):
+                dp[word] = max(dp.get(word[:i] + word[i + 1:], 0) + 1, dp.get(word, 0))
+        return max(dp.values())
+
+
+def read_input(file_name):
+    with open(file_name, 'r') as file:
+        N = int(file.readline().strip())
+        words = [file.readline().strip() for _ in range(N)]
+    return N, words
+
+
+def write_output(file_name, result):
+    with open(file_name, 'w') as file:
+        file.write(str(result) + '\n')
+
+
+def main():
+    input_file = 'wchain.in'
+    output_file = 'wchain.out'
+
+    N, words = read_input(input_file)
+    solution = Solution()
+    result = solution.longestStrChain(words)
+    write_output(output_file, result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_word_chain.py
@@ -0,0 +1,48 @@
+import unittest
+from word_chain import Solution, read_input, write_output
+
+
+class TestWChain(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_case_1(self):
+        words = ["a", "b", "ba", "bca", "bda"]
+        expected_result = 3
+        result = self.solution.longestStrChain(words)
+        self.assertEqual(result, expected_result)
+
+    def test_case_2(self):
+        words = ["xbc", "bca", "b", "bc"]
+        expected_result = 3
+        result = self.solution.longestStrChain(words)
+        self.assertEqual(result, expected_result)
+
+    def test_case_3(self):
+        words = ["a", "ab", "abc"]
+        expected_result = 3
+        result = self.solution.longestStrChain(words)
+        self.assertEqual(result, expected_result)
+
+    def test_case_4(self):
+        words = ["a", "b", "ab", "ba", "abc", "cba"]
+        expected_result = 3
+        result = self.solution.longestStrChain(words)
+        self.assertEqual(result, expected_result)
+
+    def test_case_5(self):
+        words = ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa"]
+        expected_result = 7
+        result = self.solution.longestStrChain(words)
+        self.assertEqual(result, expected_result)
+
+    def test_case_6(self):
+        words = ["a"]
+        expected_result = 1
+        result = self.solution.longestStrChain(words)
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
